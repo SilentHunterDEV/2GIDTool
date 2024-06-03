660a1c66a8ac0dc20604041e6df374fc642d38b9
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2024-06-03 14:39:21 +0200

    add version number to comment

--- 2GIDTool.cpp
+++ 2GIDTool.cpp
@@ -1,4 +1,4 @@
-// iPhone 2G ID Tool
+// iPhone 2G ID Tool v1.0
 // This tool will provide info about an iPhone 2G based on serial number
 // Idea credited to u/random_user_online
 // Initial code credited to u/randomNinja64
